73f5e6a005df40a4ed9f3039993117b6927a29f8
Rohit Gour <rohitgourrg999@gmail.com>
2023-07-04 00:52:29 +0530

    Added Validations to fields in Models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    validates :title, :body, :author, presence: true
     has_many :comments
     has_and_belongs_to_many :categories
     has_and_belongs_to_many :tags
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
+    validates :title, presence: true
     has_and_belongs_to_many :articles
 end
